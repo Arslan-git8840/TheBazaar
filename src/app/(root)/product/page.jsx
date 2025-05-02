3f1abd2aa2485b926bd6b7a2f8d9ea84abdbceb8
Arslan-git8840 <arshu09868@gmail.com>
2025-05-02 22:22:48 +0530

    useParamsFixed

--- src/app/(root)/product/page.jsx
+++ src/app/(root)/product/page.jsx
@@ -1,40 +1,16 @@
-'use client';
-import ProductSection from "@/components/root/product/ProductSection";
-import { popularProducts } from "@/data/mockData";
-import { useSearchParams } from 'next/navigation';
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const ProductClientPage = dynamic(() => import("@/components/root/product/ProductList"), {
+  
+});
+
 export default function ProductPage() {
-  const searchParams = useSearchParams();
-  const searchQuery = searchParams.get('q'); 
-
-  const fetchProductsBySearch = async (query) => {
-    if (!query) return [];
-    const { data } = await axios.get("/api/product-search", {
-      params: { q: query },
-    });
-    if (!data.success) throw new Error(data.message);
-    return data.products;
-  };
-
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["search-products"],
-    queryFn: () => fetchProductsBySearch(searchQuery),
-    enabled: !!searchQuery, 
-  });
-
-  if (!searchQuery) return <p>Please enter a search term.</p>;
-  if (isLoading) return <p>Loading search results...</p>;
-  if (error) return <p>Error fetching search results.</p>;
-
-
   return (
-    <div>
-      <h1>Search Result for: {searchQuery}</h1>
-      <ProductSection
-              seeAllLink="/popular-products"
-              products={data}
-            />
-    </div>
+    <Suspense fallback={<p>Loading search page...</p>}>
+      <ProductClientPage />
+    </Suspense>
   );
 }
+// agar kuch error hua to productlist root/pproduct se copy krke yaha paste krdenge bs
