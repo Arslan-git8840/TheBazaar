4346f6cf4cbec3bd7c2b85a1eb296cbffa240078
Arslan-git8840 <arshu09868@gmail.com>
2025-05-08 20:22:53 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-        domains: ["images.unsplash.com"],
-    }
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
